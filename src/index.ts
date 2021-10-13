7852ad4ed0674ecb909f4a7a205fec288b6d7320
anthony-freitas <78743473+anthony-freitas@users.noreply.github.com>
2021-10-13 12:48:38 -0300

    adding string to main class
    i forgot, help me

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@
 
 // Imports
 import * as numbers from './utils/numbers.js'
+import * as string from './utils/string.js'
 import * as handle from './utils/handle.js'
 import * as guards from './utils/guards.js'
 import * as types from './utils/types.js'
@@ -22,6 +23,7 @@ import * as sets from './utils/sets.js'
 export default class Miscellaneous {
   // Miscellaneous Set
   numbers = numbers
+  string = string
   handle = handle
   guards = guards
   types = types
