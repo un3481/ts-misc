fe70655e08a5e13fd9d1dff6950346a337b70549
rendezvous <78743473+un3481@users.noreply.github.com>
2022-12-14 17:44:16 -0300

    Update guards.test.ts

--- test/guards.test.ts
+++ test/guards.test.ts
@@ -1,6 +1,7 @@
 import { is } from '../src/modules/guards'
 
 describe('test SuperGuard', () => {
+  // String
   test('test SuperGuard "string"', () => {
     expect( is.string(null)            ).toBe( false );
     expect( is.string({})              ).toBe( false );
@@ -12,5 +13,18 @@ describe('test SuperGuard', () => {
     expect( is.string(String('1'))     ).toBe( true  );
     expect( is.string(new String(1))   ).toBe( true  );
     expect( is.string(new String('1')) ).toBe( true  );
+  });
+  // Number
+  test('test SuperGuard "number"', () => {
+    expect( is.number(null)            ).toBe( false );
+    expect( is.number({})              ).toBe( false );
+    expect( is.number([])              ).toBe( false );
+    expect( is.number(1)               ).toBe( false );
+    expect( is.number('1')             ).toBe( true  );
+    expect( is.number(0)               ).toBe( true  );
+    expect( is.number(Number(1))       ).toBe( true  );
+    expect( is.number(Number('1'))     ).toBe( true  );
+    expect( is.number(new Number(1))   ).toBe( true  );
+    expect( is.number(new Number('1')) ).toBe( true  );
   });
 });
