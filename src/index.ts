e35aea57510f1342622b509674dbada921376ac7
rendezvous <78743473+un3481@users.noreply.github.com>
2022-12-13 21:42:37 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,46 +10,45 @@
 #                                          https://github.com/un3481/ts-misc                                             #
 #                                                                                                                        #
 ##########################################################################################################################
-#                                                      MAIN MODULE                                                       #
-##########################################################################################################################
 */
 
 // Imports
-import * as logging from './utils/logging.js'
-import * as numbers from './utils/numbers.js'
-import * as string from './utils/string.js'
-import * as handle from './utils/handle.js'
-import * as guards from './utils/guards.js'
-import * as types from './utils/types.js'
-import * as sync from './utils/sync.js'
-import * as sets from './utils/sets.js'
+import * as _logging from './utils/logging.js'
+import * as _numbers from './utils/numbers.js'
+import * as _string from './utils/string.js'
+import * as _handle from './utils/handle.js'
+import * as _guards from './utils/guards.js'
+import * as _types from './utils/types.js'
+import * as _sync from './utils/sync.js'
+import * as _sets from './utils/sets.js'
 
-/*
-##########################################################################################################################
-#                                                  MISCELLANEOUS CLASS                                                   #
-##########################################################################################################################
-*/
+// ##########################################################################################################################
+
+// Exports
+export const logging = _logging
+export const numbers = _numbers
+export const string = _string
+export const handle = _handle
+export const guards = _guards
+export const types = _types
+export const sync = _sync
+export const sets = _sets
+
+// Super-Guard
+export const is = guards.is
 
 // Miscellaneous Class
-export default class Miscellaneous {
-  // Miscellaneous Set
-  logging = logging
-  numbers = numbers
-  string = string
-  handle = handle
-  guards = guards
-  types = types
-  sync = sync
-  sets = sets
-
-  // Allow Info Inside Misc
-  get misc(): Miscellaneous {
-    return this
-  }
+export default {
+  logging: logging,
+  numbers: numbers,
+  string: string,
+  handle: handle,
+  guards: guards,
+  types: types,
+  sync: sync,
+  sets: sets,
+  is: is
 }
 
-/*
-##########################################################################################################################
-#                                                         END                                                            #
-##########################################################################################################################
-*/
+// ##########################################################################################################################
+
